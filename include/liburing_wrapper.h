ece7ce111d2915b9c321df138d70b1599e0a97e8
Christian Mazakas <christian.mazakas@gmail.com>
2025-02-28 19:12:11 -0800

    remove unnecessary header

--- include/liburing_wrapper.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Christian Mazakas
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#pragma once
-
-#define IOURINGINLINE
-#include <liburing.h>
