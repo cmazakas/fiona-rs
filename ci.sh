db9fcd0c47474ce03b2e576fd7d857bfed0a4a7e
Christian Mazakas <christian.mazakas@gmail.com>
2026-06-21 06:43:32 -0700

    cleanup ci script

--- ci.sh
+++ ci.sh
@@ -2,19 +2,31 @@
 
 set -ex
 
-export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer-19
-export MSAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer-19
-export ASAN_OPTIONS="detect_leaks=1:detect_invalid_pointer_pairs=2:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1"
+export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer-22
+export MSAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer-22
+export ASAN_OPTIONS=
+ASAN_OPTIONS+="detect_leaks=1:detect_invalid_pointer_pairs=2:strict_string_checks=1:"
+ASAN_OPTIONS+="detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1"
 export LSAN_OPTIONS="suppressions=suppr.txt"
 
 clear
 
 CARGO_FLAGS=(--target x86_64-unknown-linux-gnu -Zbuild-std --tests)
 
-RUSTFLAGS='-Zsanitizer=address' cargo test "${CARGO_FLAGS[@]}" --profile=release-with-debug --features=sanitizers -- --test-threads=1 --nocapture
-RUSTFLAGS='-Zsanitizer=thread' cargo test "${CARGO_FLAGS[@]}" --profile=release-with-debug -- --test-threads=1 --nocapture
+echo $ASAN_OPTIONS
+
+RUSTFLAGS='-Zsanitizer=address' \
+cargo test "${CARGO_FLAGS[@]}" --profile=release-with-debug --features=sanitizers -- --test-threads=1 --nocapture
+
+RUSTFLAGS='-Zsanitizer=thread' \
 cargo test "${CARGO_FLAGS[@]}" --profile=release-with-debug -- --test-threads=1 --nocapture
 
-RUSTFLAGS='-Zsanitizer=address' cargo test "${CARGO_FLAGS[@]}" --profile=lto --features=sanitizers -- --test-threads=1 --nocapture
-RUSTFLAGS='-Zsanitizer=thread' cargo test "${CARGO_FLAGS[@]}" --profile=lto -- --test-threads=1 --nocapture
+cargo test "${CARGO_FLAGS[@]}" --profile=release-with-debug -- --test-threads=1 --nocapture
+
+RUSTFLAGS='-Zsanitizer=address' \
+cargo test "${CARGO_FLAGS[@]}" --profile=lto --features=sanitizers -- --test-threads=1 --nocapture
+
+RUSTFLAGS='-Zsanitizer=thread' \
+cargo test "${CARGO_FLAGS[@]}" --profile=lto -- --test-threads=1 --nocapture
+
 cargo test "${CARGO_FLAGS[@]}" --profile=lto -- --test-threads=1 --nocapture
